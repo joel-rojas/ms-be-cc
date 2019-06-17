95d1aa8fd87b2ea4a3a8d159798259ed397601a8
Emerson Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2019-06-17 19:12:24 -0400

    feature: Implement HTTPS certificates to load the app in HTTPS protocol. fix: Read private and non-disclosure data within an environment variable file through dotenv npm package

---
+++ config.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+module.exports = {
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+    serverUrl: process.env.SERVER_URL,
+    serverPort: process.env.SERVER_PORT,
+};
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 "use strict";
 
-const config = require('./config.json');
+const {serverUrl, serverPort} = require('./config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+const keyPath = path.join(__dirname, '..', 'certs', 'server.key');
+const certPath = path.join(__dirname, '..', 'certs', 'server.cert');
 const routes = require('./routes');
 
 const app = express();
 
-const server = require('http').createServer(app);
+const server = require('https').createServer({
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)
+}, app);
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cors());
@@ -17,8 +24,8 @@ app.options('*', cors());
 routes(app);
 
 // Start server
-server.listen(config.port, config.serverUrl, function () {
-    console.log('Express server listening on %d', config.port);
+server.listen(serverPort, serverUrl, function () {
+    console.log('Express server listening on %d', serverPort);
 });
   
 module.exports = app;
