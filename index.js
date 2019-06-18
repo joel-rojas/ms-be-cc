82e4352ce8c9ad24ad76308c882bdf67905d86ae
Emerson Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2019-06-18 10:15:15 -0400

    fix: Set server connection with HTTP protocol.

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const {serverUrl, serverPort, consumerKey,
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
-const keyPath = path.join(__dirname, '..', 'certs', 'server.key');
-const certPath = path.join(__dirname, '..', 'certs', 'server.cert');
 const routes = require('./routes');
 
 const app = express();
 
-const server = require('https').createServer({
-    key: fs.readFileSync(keyPath),
-    cert: fs.readFileSync(certPath)
-}, app);
+const server = require('http').createServer(app);
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cors());
