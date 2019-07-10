5212490d0ced6fff884ffcf07ae38d8f5749847f
Emerson Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2019-07-10 19:21:48 -0400

    fix: Remove unnecesary server response headers

--- routes.js
+++ routes.js
@@ -1,14 +1,6 @@
 'use strict';
 
 module.exports = function(app, twitterClientParams) {
-
-    app.use('*', (req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Credentials", "true");
-      res.header("Access-Control-Allow-Methods", "GET");
-      res.header("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-      next();
-    });
   
     app.use('/twitter', (req, res, next) => {
       req.twitterClient = twitterClientParams;
