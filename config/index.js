d7638c407cdd527670c4e6736b6cf8103a9b52f4
Emerson Joel Rojas Soliz <joel.rojas.soliz@gmail.com>
2019-07-15 19:41:41 -0400

    fix: Modify Twitter user statuses API calls to be called in parallel. Decrease timeout response for each API call. Append JSHint file to enable new JS syntax rules

--- config/index.js
+++ config/index.js
@@ -1,10 +1,13 @@
+"use strict";
+
 const dotenv = require('dotenv');
 dotenv.config();
+
 module.exports = {
     consumerKey: process.env.CONSUMER_KEY,
     consumerSecret: process.env.CONSUMER_SECRET,
     accessToken: process.env.ACCESS_TOKEN,
     accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
     serverUrl: process.env.SERVER_URL,
-    serverPort: process.env.SERVER_PORT,
+    serverPort: process.env.SERVER_PORT
 };
